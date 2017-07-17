c14f5ab9c61170945dd25976d811e17a20c29a12
Josue Alvarez <josuetec2003@gmail.com>
2017-07-17 15:52:26 -0600

    Two way data-binding en toda la tabla y estilo a empleado seleccionado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,19 +9,22 @@ import { Empleado } from '../models/empleado';
 
 export class AppComponent {
 	public empleado:Empleado;
+	public empleadoActual:Empleado;	
 	public mostrarDatos:boolean;
 	public empleados:Array<Empleado>;
 
 	constructor ()
 	{
-		this.mostrarDatos = true;
-		this.empleado = new Empleado('0', 'Jack', 'Dorsey', 40000);
+		this.mostrarDatos = true;		
 
 		this.empleados = [
+			new Empleado('0', 'Jack', 'Dorsey', 40000),
 			new Empleado('1', 'Mark', 'Zuck', 10000),
 			new Empleado('2', 'Jeff', 'Bezzos', 5000),
 			new Empleado('3', 'Sergey', 'Brin', 15000)
 		];
+
+		this.empleado = this.empleados[0];
 
 		this.debug();
 	}
@@ -29,6 +32,12 @@ export class AppComponent {
 	mostrar ()
 	{
 		this.mostrarDatos = !this.mostrarDatos;
+	}
+
+	editarEmpleado (emp:Empleado)
+	{
+		this.empleado = emp;
+		this.empleadoActual = this.empleado;
 	}		
 
 	debug ()
